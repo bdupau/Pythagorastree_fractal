591bb0fd82192c85cbf62b7dd768c15e8403a50e
bdupau <gcsdupau@gmail.com>
2016-01-24 12:43:05 +0100

    First success !

--- script.js
+++ script.js
@@ -1,52 +1,80 @@
-
 shrink = 0.5*Math.sqrt(2);
 
 var c = document.getElementById("myCanvas");
 var drawing = c.getContext("2d");
 
 var length = 100;
-var stop = 50;
-drawing.fillStyle = "#ff9900";
+var stop = 1;
+drawing.fillStyle = "#995500";
+
+function turn(direction){
+  // Turns one eighth of a circle
+  if(direction=="right"){
+    drawing.rotate(Math.PI/4);
+  };
+  if(direction=="left"){
+    drawing.rotate(-Math.PI/4);
+  };
+};
+
+function getColor(length){
+  var color = Math.floor(length/shrink)+10;
+  if(color<10){
+    color = "0"+color;
+  };
+  return color;
+};
 
 //origin van canvas op 300,300 zetten
-drawing.translate(300,300);
-drawing.rotate(-Math.PI/4);
+drawing.translate(400,400);
+turn("left");
 
 // Misschien moeten we wel twee recursion functies maken,
 // Eentje voor links doorgaan, en eentje voor rechts doorgaan ?
 function recursion(length){
-	// Teken de linkse
- 	drawing.rotate(-Math.PI/4);
- 	drawing.fillRect(0,0,shrink*length,shrink*length);
-
- 	// Ga door links
-  if(length > stop){
-    drawing.translate(shrink*length,0);
-    recursion(shrink*length);
-	};
+  length = shrink*length;
   
-	// Teken de rechtse
-	drawing.rotate(Math.PI/4);
-	drawing.translate(0,length);
-	drawing.rotate(-Math.PI/4);
- 	drawing.fillRect(0,0,shrink*length,shrink*length);
+  // Teken de linkse
+  turn("left");
+  drawing.fillStyle = "#"+getColor(length)+"5500";
+  drawing.fillRect(0,0,length,length);
+  
+  // Ga door links
+  if(length > stop){
+    drawing.translate(length,0);
+    recursion(length);
     
+    // Ga terug
+    turn("right");
+    drawing.translate(-length,-length);
+  };
+     
+  // Teken de rechtse
+  turn("right");
+  drawing.translate(0,length/shrink);
+  turn("left");
+  drawing.fillStyle = "#"+getColor(length)+"5500";
+  drawing.fillRect(0,0,length,length);
+ 
   // Ga door rechts
   if(length > stop){
-    drawing.translate(shrink*length,0);
-    drawing.rotate(Math.PI/2);
-    drawing.translate(shrink*length,0);
-    recursion(shrink*length);
+    drawing.translate(length,0);
+    turn("right");
+    turn("right");
+    drawing.translate(length,0);
+    recursion(length);
+    
+    // Ga terug
+    turn("right");
+    drawing.translate(-length,0);
+    turn("left");
+    turn("left");
   };
-  
-  // Ga terug
-  drawing.rotate(Math.PI/4);
-  drawing.translate(-length,-length);
 };
 
 // Teken de eerste
 drawing.translate(length,0);
-drawing.rotate(-Math.PI/4);
+turn("left");
 drawing.fillRect(0,0,shrink*length,shrink*length);
 drawing.translate(shrink*length,0);
 
